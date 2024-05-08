70be9cc9223988de2ef8bcfae187477e42d664ec
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2024-05-08 13:57:52 +0200

    Remove python backend

--- backend/dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-watchmedo auto-restart --pattern  "*.py" --recursive --signal SIGTERM python3 server.py
--- dev.sh
+++ dev.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-set -e
-
-id=$(kitty @ new-window --cwd $(pwd))
-kitty @ send-text -m id:$id '(cd backend; ./dev.sh)\n'
-kitty @ goto-layout vertical
-
-(cd frontend; ./dev.sh)
+npm run dev -- --open
